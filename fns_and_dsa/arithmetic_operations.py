6792df04fac9a0b8a0f0360e4e50087a74c17cf3
Nebyou Damtew <nebyoudamtew18@gmail.com>
2024-09-27 20:48:46 +0300

    Functions, Data Structures and Modules learning tasks

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,8 @@
 def perform_operation(num1,num2,operation):
+    print("Arithmetic Operations")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
    
     match operation:
         case "add":
