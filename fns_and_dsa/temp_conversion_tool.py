a4f5158ccf5fdfe49443ef474c86a35cf218c7bf
Nebyou Damtew <nebyoudamtew18@gmail.com>
2024-09-27 21:02:44 +0300

    added global varibles to the functions.

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -6,7 +6,7 @@ def convert_to_celsius(fahrenheit):
     return FAHRENHEIT_TO_CELSIUS_FACTOR*(fahrenheit - 32)
 def convert_to_fahrenheit(celsius):
     global CELSIUS_TO_FAHRENHEIT_FACTOR
-    return CELSIUS_TO_FAHRENHEIT_FACTOR*celsius + 32
+    return (CELSIUS_TO_FAHRENHEIT_FACTOR*celsius) + 32
 
 
 temperature = int(input("Enter the temperature to convert: "))
