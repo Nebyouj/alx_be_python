2ee0f8811f73bc2e11adc391aa152f72f8c6de07
Nebyou Damtew <nebyoudamtew18@gmail.com>
2024-09-27 21:19:32 +0300

    added global varibles to the functions.

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,17 +2,20 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-    return FAHRENHEIT_TO_CELSIUS_FACTOR*(fahrenheit - 32)
+    celsius = FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit - 32)
+    return celsius
+
 def convert_to_fahrenheit(celsius):
-    return (CELSIUS_TO_FAHRENHEIT_FACTOR*celsius) + 32
+    fahrenheit = (CELSIUS_TO_FAHRENHEIT_FACTOR * celsius) + 32
+    return fahrenheit
 
 
 temperature = int(input("Enter the temperature to convert: "))
 unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").lower()
 match unit:
     case "c":
-        print(f"{temperature}°C is {convert_to_fahrenheit(temperature)}°F")
+        print(f"{temperature}°C is {convert_to_fahrenheit(temperature)}°F") if temperature > -273.15 else print("Invalid temperature")
     case "f":
-        print(f"{temperature}°F is {convert_to_celsius(temperature)}°C")
+        print(f"{temperature}°F is {convert_to_celsius(temperature)}°C") if temperature > -459.67 else print("Invalid temperature")
     case"_":
         print("Invalid unit of measurement")
