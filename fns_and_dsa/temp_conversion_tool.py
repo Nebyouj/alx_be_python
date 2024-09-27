11580cf43d4dc9db5df325ef4c0ad57e7db2ea5b
Nebyou Damtew <nebyoudamtew18@gmail.com>
2024-09-27 22:03:17 +0300

    added a try/except to ensure numeric values only.

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -8,13 +8,17 @@ def convert_to_fahrenheit(celsius):
     return (CELSIUS_TO_FAHRENHEIT_FACTOR + 32) * celsius
      
 
+try:
+    temperature = float(input("Enter the temperature to convert: "))
+    if not temperature.replace('.','',1).isdigit():
+        raise ValueError("Invalid temperature. Please enter a numeric value.") 
+    unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").lower()
 
-temperature = float(input("Enter the temperature to convert: "))
-unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").lower()
-
-if unit == "c":
-    print(f"{temperature}°C is {convert_to_fahrenheit(temperature)}°F") if temperature > -273.15 else print("Invalid temperature")
-elif unit == "f":
-    print(f"{temperature}°F is {convert_to_celsius(temperature)}°C") if temperature > -459.67 else print("Invalid temperature")
-else:
-    print("Invalid unit of measurement")
+    if unit == "c":
+        print(f"{temperature}°C is {convert_to_fahrenheit(temperature)}°F") if temperature > -273.15 else print("Invalid temperature")
+    elif unit == "f":
+        print(f"{temperature}°F is {convert_to_celsius(temperature)}°C") if temperature > -459.67 else print("Invalid temperature")
+    else:
+        print("Invalid unit of measurement")
+except ValueError as e:
+    print(e)
