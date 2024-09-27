850c85b937bd41551b619ce2d75d8336428696e3
Nebyou Damtew <nebyoudamtew18@gmail.com>
2024-09-27 21:23:57 +0300

    added the types to the parameters.

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,9 +1,5 @@
-def perform_operation(num1,num2,operation):
-    print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-   
+def perform_operation(num1:float,num2:float,operation:str):
+    
     match operation:
         case "add":
             return num1 + num2
