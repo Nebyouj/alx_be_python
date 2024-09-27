9985e90ef4338944eae4af4724ec3b10f3ea7633
Nebyou Damtew <nebyoudamtew18@gmail.com>
2024-09-27 21:47:09 +0300

    changed match statement to if-else statement.

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,21 +1,20 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 def convert_to_celsius(fahrenheit):
-    celsius = FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit - 32)
-    return celsius
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
-    fahrenheit = (CELSIUS_TO_FAHRENHEIT_FACTOR * celsius) + 32
-    return fahrenheit
+    return (CELSIUS_TO_FAHRENHEIT_FACTOR * celsius) + 32
+     
 
 
-temperature = int(input("Enter the temperature to convert: "))
+temperature = float(input("Enter the temperature to convert: "))
 unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").lower()
-match unit:
-    case "c":
-        print(f"{temperature}°C is {convert_to_fahrenheit(temperature)}°F") if temperature > -273.15 else print("Invalid temperature")
-    case "f":
-        print(f"{temperature}°F is {convert_to_celsius(temperature)}°C") if temperature > -459.67 else print("Invalid temperature")
-    case"_":
-        print("Invalid unit of measurement")
+
+if unit == "c":
+    print(f"{temperature}°C is {convert_to_fahrenheit(temperature)}°F") if temperature > -273.15 else print("Invalid temperature")
+elif unit == "f":
+    print(f"{temperature}°F is {convert_to_celsius(temperature)}°C") if temperature > -459.67 else print("Invalid temperature")
+else:
+    print("Invalid unit of measurement")
