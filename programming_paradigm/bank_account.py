dc53eae1309f435b7b4b072c779a260f59c86dea
Nebyou Damtew <nebyoudamtew18@gmail.com>
2024-10-04 22:57:34 +0300

    added initial balance

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,5 +1,5 @@
 class BankAccount:
-    def __init__(self, account_balance = 0):
+    def __init__(self, account_balance = 0.00):
         self.account_balance = account_balance
 
     def deposit(self, amount):
@@ -13,5 +13,5 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"Current Balance: ${float(self.account_balance)}")
+        print(f"Current Balance: ${self.account_balance}")
 
